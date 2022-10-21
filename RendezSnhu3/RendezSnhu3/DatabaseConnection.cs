f21f65198ba9f76cd118ac9eb7a347ffd7078e24
JustinSilvas <98841304+JustinSilvas@users.noreply.github.com>
2022-10-21 10:35:43 -0400

    Created Local host connection and database class unfinished (need to fix the password before wokrs) Extension mySql.Data needed

---
+++ RendezSnhu3/RendezSnhu3/DatabaseConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace RendezSnhu3
+{
+    class DatabaseConnection
+    {
+        static void Connect()
+        {
+            string server = "localhost"; // If not using a cloud database, use localhost. If using cloud database, use the IP to the server
+            string database = "snhu"; // Whatever database you want use for the application
+            string username = "root"; // The username of your MySQL client
+            string password = "password"; // The password of your MySQL client
+
+            // Throw all the variables into this connection string
+            // MUST USE THIS FORMAT! (server, database, username, then password)
+            string connstring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+
+        }
+    }
+}
